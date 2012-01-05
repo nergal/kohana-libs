6b26f510251c1385ac7675d0cad784930330f6c2
nergal <nergal@nergal.org.ua>
2012-01-05 19:32:07 +0300

    Zend\Registry plugin

---
+++ classes/registry.php
@@ -0,0 +1,3 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Registry extends Kohana_Registry {}
---
+++ tests/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+
+if ( ! defined('SYSPATH')) {
+    error_reporting(E_ALL | E_STRICT);
+
+    require_once 'PHPUnit/Framework/TestCase.php';
+    require_once __DIR__ . '/../classes/kohana/registry.php';
+}
